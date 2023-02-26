6d2521b9745ef821b37e957d7e481c89f69900cc
sohier <sohier.lotfy.els@gmail.com>
2023-02-26 15:34:22 +0400

    new commit

--- Day_011/blackjack.py
+++ Day_011/blackjack.py
@@ -12,7 +12,7 @@
 from blackjack_art import *
 import random
 import os
-
+#add new commit 
 def deal_cards():
     """Returns a random card from the deck."""
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
