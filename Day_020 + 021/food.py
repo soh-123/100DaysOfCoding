d92da130afac82bff3502489b4c62cbdc1db910a
sohier <sohier.lotfy.els@gmail.com>
2023-03-07 13:22:10 +0400

    Snake game

---
+++ Day_020 + 021/food.py
@@ -0,0 +1,18 @@
+from turtle import *
+import random
+
+class Food(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.color("blue")
+        self.penup()
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.speed("fastest")
+        self.refresh()
+
+    
+    def refresh(self):
+            random_x = random.randint(-280, 280)
+            random_y = random.randint(-280, 270)
+            self.goto(x=random_x, y=random_y)
---
+++ Day_020 + 021/snake_game.py
@@ -0,0 +1,43 @@
+from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import Score_Board
+import time
+
+screen = Screen()
+screen.setup(600, 600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = Score_Board()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+is_game_on = True
+while is_game_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        is_game_on = False
+        scoreboard.game_over()
+
+    for segments in snake.segment[1:]:
+        if snake.head.distance(segments) < 10:
+            is_game_on = False
+            scoreboard.game_over() 
+
+screen.exitonclick()
